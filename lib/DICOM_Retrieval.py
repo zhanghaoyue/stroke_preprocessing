4c7dc51f0464ce859393f10ae5ed4fae877a1696
zhanghaoyue <harryzhangbruins@gmail.com>
2019-08-06 13:49:29 -0700

    add first draft T2 preprocessing next step: add co-registration for other modality

--- lib/DICOM_Retrieval.py
+++ lib/DICOM_Retrieval.py
@@ -12,29 +12,28 @@ import psutil
 class DicomRetrieval:
 
     def __init__(self, out_base_dir, out_query_dir):
-
         self.out_base_dir = out_base_dir
         self.out_query_dir = out_query_dir
         self.patient_outdir = None
         self.rv = None
         self.ls = None
 
-    def listening(self, patient):
+    def listening(self, patient, f):
         self.patient_outdir = os.path.join(self.out_base_dir, patient)
         self.ls = subprocess.Popen('storescp -b bofa-420-aberle@10.9.94.219:12112 \
-         --directory %s' % self.patient_outdir, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+         --directory %s' % self.patient_outdir, shell=True, stdout=f, preexec_fn=os.setsid)
 
-    def query(self, patient, accession):
+    def query(self, patient, accession, f):
         patient_uid_dir = os.path.join(self.out_query_dir, patient, 'AccessionNumber-' + accession)
         subprocess.call('findscu -b bofa-420-aberle@10.9.94.219:12112 \
-                -c WWPACSQR@10.7.1.64:4100 -m AccessionNumber=%s -r StudyInstanceUID \
-                --out-dir %s' % (accession, patient_uid_dir), shell=True)
+                -c WWPACSQR@10.7.1.64:4100 -m AccessionNumber=%s -r StudyInstanceUID NumberOfSeriesRelatedInstances\
+                --out-dir %s' % (accession, patient_uid_dir), stdout=f, shell=True)
 
-    def retrieve(self, patient, accession, fout):
+    def retrieve(self, patient, accession, f):
         patient_uid_dir = os.path.join(self.out_query_dir, patient, 'AccessionNumber-' + accession + '/*')
         self.rv = subprocess.call('movescu -b bofa-420-aberle@10.9.94.219:12112 \
         -c WWPACSQR@10.7.1.64:4100 -i StudyInstanceUID --dest bofa-420-aberle %s' %
-                                  patient_uid_dir, stdout=fout, shell=True)
+                                  patient_uid_dir, stdout=f, shell=True)
 
     @staticmethod
     def kill(proc_pid):
@@ -60,61 +59,47 @@ if __name__ == '__main__':
 
     while i < num_iterations:
 
-        completed_patients_fname = os.path.join(out_base_dir, 'completed_patients.txt')
-        try:
-            completed_patients_in = open(completed_patients_fname, 'r')
-        except:
-            completed_patients_in = open(completed_patients_fname, 'w')
-            completed_patients_in = open(completed_patients_fname, 'r')
-
-        completed_list = []
-        for line in completed_patients_in:
-            completed_list.append(ast.literal_eval(line))
-        completed_patients_in.close()
-
-        new_list = []
-        for pt in np.arange(0, dcm_array.shape[0]):
-            if dcm_array[pt][0] not in completed_list:
-                new_list.append(dcm_array[pt])
-
-        if len(new_list) == 0:
-            break
-
-        completed_patients_out = open(completed_patients_fname, 'a')
-
         logging.info("Iter %d, Starting DICOM dump into directory %s" % (i, out_base_dir,))
         logging.info("Error logs located at %s" % log_outdir)
-        logging.info("%d studies remain" % (len(new_list),))
 
-        for pt_row in new_list:
+        for pt_row in dcm_array[0:3]:
+            t0 = time.time()
             print(pt_row)
-            retrieval_instance = DicomRetrieval(out_base_dir, out_query_dir)
-            retrieval_instance.query(patient=pt_row[0], accession=pt_row[2])
-            retrieval_instance.listening(patient=pt_row[0])
-            time.sleep(1)
-            log_out_filename = os.path.join(log_outdir, '%s.%d.log' % (str(pt_row[0]), num_iterations))
-            fout = open(log_out_filename, 'w')
-            retrieval_instance.retrieve(patient=pt_row[0], accession=pt_row[2], fout=fout)
-            print('Waiting...')
-            time.sleep(30)
-            fout.close()
-            try:
-                retrieval_instance.ls.wait(timeout=3)
-            except subprocess.TimeoutExpired:
-                retrieval_instance.kill(retrieval_instance.ls.pid)
-            print(retrieval_instance.ls.pid)
-            os.killpg(os.getpgid(retrieval_instance.ls.pid), signal.SIGTERM)
-
-            empty_dirs = [dirpath for (dirpath, dirnames, filenames) in os.walk(retrieval_instance.patient_outdir) if
-                          len(filenames) == 0 and len(dirnames) == 0]
-            if len(empty_dirs) == 0:
-                completed_patients_out.write('%s\n' % (pt_row[0],))
+            if not os.path.isdir(out_base_dir+'/'+pt_row[0]):
+                os.makedirs(out_base_dir+'/'+pt_row[0])
+            path, dir, files = next(os.walk(out_base_dir+'/'+pt_row[0]))
+            if len(files) < int(pt_row[4]):
+                log_out_query = os.path.join(log_outdir, '%s.%d.query.log' % (str(pt_row[0]), i))
+                log_out_listen = os.path.join(log_outdir, '%s.%d.listen.log' % (str(pt_row[0]), i))
+                log_out_retrieve = os.path.join(log_outdir, '%s.%d.retireve.log' % (str(pt_row[0]), i))
+                query_log = open(log_out_query, 'w')
+                listening_log = open(log_out_listen, 'w')
+                retrieve_log = open(log_out_retrieve, 'w')
+                retrieval_instance = DicomRetrieval(out_base_dir, out_query_dir)
+                retrieval_instance.query(patient=pt_row[0], accession=pt_row[2], f=query_log)
+                retrieval_instance.listening(patient=pt_row[0], f=listening_log)
+                print("port listening process PID:")
+                print(retrieval_instance.ls.pid)
+                print("start retrieving")
+                time.sleep(1)
+                retrieval_instance.retrieve(patient=pt_row[0], accession=pt_row[2], f=retrieve_log)
+                t1 = time.time()
+                print('retrieval time used %d ' % (t1 - t0))
+                print('Waiting...30s')
+                time.sleep(30)
+                query_log.close()
+                listening_log.close()
+                retrieve_log.close()
+                '''
+                try:
+                    retrieval_instance.ls.wait(timeout=1)
+                except subprocess.TimeoutExpired:
+                    retrieval_instance.kill(retrieval_instance.ls.pid)
+                '''
+                os.killpg(os.getpgid(retrieval_instance.ls.pid), signal.SIGTERM)
             else:
-                for ed in empty_dirs:
-                    logging.error("Failure rv(%d) for patient %s" % (retrieval_instance.rv, pt_row[0]))
-                    print("Failure rv(%d) for patient %s" % (retrieval_instance.rv, pt_row[0]))
-                    os.rmdir(ed)
-        completed_patients_out.close()
+                continue
 
-        num_iterations += 1
+        i += 1
+
     logging.info("Dump complete")
