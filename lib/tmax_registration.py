d69ffcb71b67da007ef2e25b91bfc04b84cfe2f4
Harry Zhang <harryzhangbruins@gmail.com>
2023-06-28 12:21:11 -0400

    some minor updates for tmax co-registration

--- lib/tmax_registration.py
+++ lib/tmax_registration.py
@@ -1,5 +1,5 @@
 import os
-from nipype.interfaces.ants import Registration, ApplyTransforms
+from nipype.interfaces.ants import Registration, ApplyTransforms, N4BiasFieldCorrection
 from fsl.wrappers import robustfov, fslreorient2std
 import subprocess
 
@@ -23,11 +23,31 @@ test_dir = "G:/ResearchStroke/no-reflow/sample_case/"
 # rf_out_roi = os.path.join(test_dir, '0599875_post_FLAIR_RF.nii.gz')
 # robustfov(rf_in_file, rf_out_roi)
 
+# n4_1 = N4BiasFieldCorrection()
+# n4_1.inputs.dimension = 3
+# n4_1.inputs.input_image = os.path.join(test_dir, '0599875_pre_FLAIR.nii.gz')
+# n4_1.inputs.bspline_fitting_distance = 300
+# n4_1.inputs.shrink_factor = 3
+# n4_1.inputs.n_iterations = [50, 50, 30, 20]
+# n4_1.inputs.output_image = os.path.join(test_dir, '0599875_pre_FLAIR_n4.nii.gz')
+# subprocess.call(n4_1.cmdline, shell=True)
+#
+# n4_2 = N4BiasFieldCorrection()
+# n4_2.inputs.dimension = 3
+# n4_2.inputs.input_image = os.path.join(test_dir, '0599875_post_FLAIR.nii.gz')
+# n4_2.inputs.bspline_fitting_distance = 300
+# n4_2.inputs.shrink_factor = 3
+# n4_2.inputs.n_iterations = [50, 50, 30, 20]
+# n4_2.inputs.output_image = os.path.join(test_dir, '0599875_post_FLAIR_n4.nii.gz')
+# subprocess.call(n4_2.cmdline, shell=True)
+#
+# print('N4 Bias Field Correction running...')
+
 print('ANTs registration...')
 reg = Registration()
 # reg = RegistrationSynQuick()
-reg.inputs.fixed_image = os.path.join(test_dir, '5492881_pre_FLAIR.nii.gz')
-reg.inputs.moving_image = os.path.join(test_dir, '5492881_post_FLAIR.nii.gz')
+reg.inputs.fixed_image = os.path.join(test_dir, '0599875_pre_FLAIR_n4_stripped.nii.gz')
+reg.inputs.moving_image = os.path.join(test_dir, '0599875_post_FLAIR_n4_stripped.nii.gz')
 reg.inputs.output_transform_prefix = os.path.join(test_dir, '5492881_transform.mat')
 reg.inputs.winsorize_upper_quantile = 0.995
 reg.inputs.winsorize_lower_quantile = 0.005
@@ -49,7 +69,7 @@ reg.inputs.smoothing_sigmas = [[4, 2, 1]] * 3 + [[1, 0.5, 0]]
 reg.inputs.sigma_units = ['vox'] * 4
 reg.inputs.shrink_factors = [[6, 4, 2]] + [[3, 2, 1]] * 2 + [[4, 2, 1]]
 reg.inputs.use_histogram_matching = [False] * 3 + [True]
-reg.inputs.output_warped_image = os.path.join(test_dir, '5492881_post_FLAIR_r.nii.gz')
+reg.inputs.output_warped_image = os.path.join(test_dir, '0599875_post_FLAIR_r.nii.gz')
 reg.inputs.initial_moving_transform_com = True
 reg.inputs.verbose = True
 print(reg.cmdline)
@@ -59,11 +79,11 @@ print('ANTs registration done...')
 print('ANTs apply transform...')
 at = ApplyTransforms()
 at.inputs.dimension = 3
-at.inputs.input_image = os.path.join(test_dir, '5492881_post_TMAX.nii.gz')
-at.inputs.reference_image = os.path.join(test_dir, '5492881_post_FLAIR_r.nii.gz')
-at.inputs.output_image = os.path.join(test_dir, '5492881_post_TMAX_r.nii.gz')
+at.inputs.input_image = os.path.join(test_dir, '0599875_post_TMAX.nii.gz')
+at.inputs.reference_image = os.path.join(test_dir, '0599875_post_FLAIR_r.nii.gz')
+at.inputs.output_image = os.path.join(test_dir, '0599875_post_TMAX_r.nii.gz')
 at.inputs.default_value = 0
-at.inputs.transforms = os.path.join(test_dir, '5492881_transform.matComposite.h5')
+at.inputs.transforms = os.path.join(test_dir, '0599875_transform.matComposite.h5')
 print(at.cmdline)
 subprocess.call(at.cmdline, shell=True)
 print('ANTs apply transform done...')
